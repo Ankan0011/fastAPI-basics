96b6559c22eec1770c6c19d612b3726e75224897
Ankan0011 <ankan.ghosh@uzh.ch>
2022-05-12 15:12:28 +0200

    payment Api changes

---
+++ payment/main.py
@@ -0,0 +1,78 @@
+import time
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.background import BackgroundTasks
+from redis_om import get_redis_connection, HashModel
+from starlette.requests import Request
+import requests, time
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+# Use a different DB instance for the connnection.
+redis = get_redis_connection(
+    host="redis-11138.c135.eu-central-1-1.ec2.cloud.redislabs.com",
+    port=11138,
+    password="W9MK79gNircfMwsmw3FL74qMObLiXIuo",
+    decode_responses=True
+)
+
+class Order(HashModel):
+    product_id: str
+    price: float
+    fee: float
+    total: float
+    quantity: int
+    status: str # This should be an enum[pending, completed, refunded]
+
+    class Meta:
+        database = redis
+
+@app.get('/orders/{pk}')
+def get(pk : str):
+    return Order.get(pk)
+
+
+@app.post('/orders')
+async def create(request: Request, background_tasks: BackgroundTasks): # id, quantity
+    body = await request.json()
+
+    # Https get request to the other service for internal communication
+    # Use pip install request package
+    req = requests.get('http://localhost:8000/products/%s' % body['id'])
+
+    product = req.json()
+    order = Order(
+        product_id = body['id'],
+        price = product['price'],
+        fee = 0.2 * product['price'],
+        total = 1.2 * product['price'],
+        quantity = body['quantity'],
+        status = 'pending'
+    )
+    order.save()
+    background_tasks.add_task(order_completed,order)
+    return order
+
+
+def order_completed(order: Order):
+    time.sleep(5)
+    order.status = 'completed'
+    order.save()
+    redis.xadd('order_completed', order.dict(), '*')
+
+
+
+
+
+
+
+
+
